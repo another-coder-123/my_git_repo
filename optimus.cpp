4bdcc59d65707113c34f2899240a3b2dadf96a4d
Mika Dan <mikadan19@gmail.com>
2025-06-30 18:51:04 +0300

    resolve pr comments

--- optimus.cpp
+++ optimus.cpp
@@ -1,31 +1,65 @@
 #include <iostream>
+#include <string>
+#include <string_view>
 
-int is_prime(int num)
-/**
-* Returnes 1 (True) if the given number is prime
-* 0 if it's not prime
-*/
+class NegativeNumberException : public std::exception
 {
-	for (int i{ 2 }; i < num; ++i)
+private:
+	std::string m_error;
+public:
+	NegativeNumberException(const std::string& error) : m_error(error) {}
+	const std::string& getError() const {
+		return m_error;
+	}
+};
+
+class InvalidInputException : public std::exception
+{
+private:
+	std::string m_error;
+public:
+	InvalidInputException(const std::string& error) : m_error(error) {}
+	const std::string& getError() const {
+		return m_error;
+	}
+};
+
+//Returns true if the given number is prime, false if not
+bool is_prime(int num)
+{
+	if (num < 0) {
+		throw NegativeNumberException("Negative number cannot be prime!");
+	}
+	for (int i = 2; i < num; ++i)
 	{
 		if ((num % i) == 0)
 		{
-			return 0;
+			return false;
 		}
 	}
-	return 1;
+	return true;
 }
 
 int main()
 {
+	int div_result;
 	int number;
 	int prime_flag;
+	
 	std::cout << "Enter a number: ";
-	std::cin >> number;
-	std::cout << "Checking if " << number << " is prime...\n";
-	prime_flag = is_prime(number);
-	if (prime_flag == 1)
-		std::cout << "It is Prime!\n";
-	else 
-		std::cout << "This number is not Prime!\n";
+	if (std::cin >> number) {
+		std::cout << "Checking if " << number << " is prime...\n";
+		prime_flag = is_prime(number);
+		if (prime_flag)
+		{
+			std::cout << "It is Prime!\n";
+		}
+		else {
+			std::cout << "This number is not Prime!\n";
+		}
+	}
+	else {
+		throw InvalidInputException("Invalid Input! input should be a whole positive number");
+	}
+	return 0;
 }
